ec2818a6a3cd7f96ec8ac94bd40e47bd6028d0c5
orikalin <orikalin@gmail.com>
2025-07-25 02:40:36 -0400

    preparing for state machine

--- Assets/Scripts/base_body.gd
+++ Assets/Scripts/base_body.gd
@@ -16,17 +16,13 @@ func _process(_delta:float):
 	if PlayerControllerNode != null:
 		target_speed = PlayerControllerNode.target_speed
 		if EngineLights.size() > 0:
-			var EnginePower = _Map(target_speed, 0, PlayerControllerNode.max_flight_speed, 0, 1)
+			var EnginePower = Helpers.Map(target_speed, 0, PlayerControllerNode.max_flight_speed, 0, 1)
 			var _EngineCurveSample:float = EngineLightCurve.sample(EnginePower)
 			for light:SpotLight3D in EngineLights:
 				light.spot_attenuation = lerp(0.5, EngineLightIntensity, _EngineCurveSample)
 				light.light_energy = lerp(1.0, EngineLightEnergy, _EngineCurveSample) 
 				
 func _physics_process(delta: float) -> void:pass
-	
-			
-func _Map(val:float, min1:float, max1:float, min2:float, max2:float) -> float:
-	return  min2 + (max2 - min2) * ((val - min1) / (max1 - min1))
 
 	
 	
