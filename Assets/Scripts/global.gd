9244dda60f75655c6bed47bf27b077987aa1006a
orikalin <orikalin@gmail.com>
2025-08-01 01:23:25 -0400

    started on refactor
    refactoring the main scene as a Game Manager, with event manager, scene manager, and player manager

---
+++ Assets/Scripts/global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var scene_manager:SceneManager
+var player_manager:PlayerManager
+var signal_manager
--- Prefab scenes/input.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-@export var turn_input:float = 0.0
-@export var target_speed:float = 0.0
-@export var pitch_input:float = 0.0
